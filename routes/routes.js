76fe046873aa3dbdb8dd0ad4687dd9bb0d80ea22
Spencer Pollock <spencer@spollock.ca>
2020-04-22 15:03:51 -0700

    feat(routes/routes.js): added 2 functions to create a single user or a list of users

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,38 @@
+const faker = require("faker");
+
 const appRouter = (app) => {
     app.get('/',  (req, res) => {
         res.status(200).send("Welcome to a restful API");
+    });
+
+    // NOTE: Creates a single user
+    app.get('/user', (req, res) => {
+        var data = ({
+            firstName: faker.name.firstName(),
+            lastname: faker.name.lastName(),
+            username: faker.internet.userName(),
+            email: faker.internet.email()
+        });
+        res.status(200).send(data);
+    });
+
+    // NOTE: Creates a list of users equal to the number entered.
+    app.get('/users/:num', (req, res) => {
+        let users = [];
+        let num = req.params.num;
+        if (isFinite(num) && num > 0) {
+            for(i = 0; i <= num; i++) {
+                users.push({
+                    firstname: faker.name.firstName(),
+                    lastname: faker.name.lastName(),
+                    username: faker.internet.userName(),
+                    email: faker.internet.email()
+                });
+            }
+            res.status(200).send(users);
+        } else {
+            res.status(400).send({ message: "invalid user number"});
+        }
     });
 }
 
