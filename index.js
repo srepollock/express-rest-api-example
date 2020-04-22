953b476e16937ed9bd3ded4f746003bce5ccf3fc
Spencer Pollock <spencer@spollock.ca>
2020-04-22 14:51:03 -0700

    feat(index.js): going with a simpler example

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const routes = require("./routes/routes.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world, from express");
-});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.listen(port);
-console.log(`Magic happens on port ${port}`);
+routes(app);
+
+var server = app.listen(port, () => {
+    console.log(`Magic happens on port ${server.address().port}`);
+});
